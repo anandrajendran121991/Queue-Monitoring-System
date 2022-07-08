1e4d7f5fcbe475d393b2c813dbdd70639ab8242c
Anand Rajendran <102396797+imadevguyanand@users.noreply.github.com>
2022-07-07 23:23:04 -0400

    Delete conf.sh

--- local/conf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Local Variables
-export HOST_ADDRESS=docker.for.mac.localhost
-export APP_PORT_PREFIX=57
-export MOUNT_DIR=/Users/anandrajendran
-export APP_DIR=/Users/anandrajendran/Documents/Projects/queue-system
